92d22551230220a78aaa97454a06a00d2f7e1b18
Andrew Arther <9995126@student.toiohomai.ac.nz>
2019-07-31 11:36:04 +1200

    2nd week programming

---
+++ 2nd Week Programming/2nd Week Programming/2nd Week Programming/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_Week_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
